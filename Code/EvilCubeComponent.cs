ac7346394887d88e1de40246b489fdeadb6649de
CYKR <cykreye@gmail.com>
2025-03-31 20:44:02 +0200

    Mise à jour des propriétés de la scène et ajout d'un composant
    Modifications apportées au fichier `minimal.scene` :
    - Mise à jour des couleurs de lumière et de ciel.
    - Changement de la projection de "Sphere" à "Box".
    - Mise à jour des textures et ajout d'un nouvel objet avec un guid différent.
    - Remplacement d'un `BoxCollider` par un composant `EvilCubeComponent` avec des propriétés pour gérer la santé et l'équipe.
    
    Ajout du fichier `EvilCubeComponent.cs` :
    - Création d'une énumération `TeamType` pour définir les équipes.
    - Définition des propriétés pour le nom, l'équipe et la santé maximale.
    - Implémentation des méthodes de cycle de vie pour gérer le comportement du composant.

--- Code/EvilCubeComponent.cs
+++ Code/EvilCubeComponent.cs
@@ -1,9 +1,86 @@
+﻿using System.Linq.Expressions;
 using Sandbox;
 
+/// <summary>
+/// All available teams
+/// </summary>
+public enum TeamType
+{
+	[Icon("🔵")]
+	[Description( "Team blue is the best" )]
+	Blue,
+	[Icon( "🔴" )]
+	[Description( "Red for ever !" )]
+	Red,
+}
 public sealed class EvilCubeComponent : Component
 {
+	// Properties
+
+
+	/// <summary>
+	/// Name of the entity
+	/// </summary>
+	[Property]
+	public string Name { get; set; }
+
+	/// <summary>
+	/// Team of the entity
+	/// </summary>
+	[Property]
+	public TeamType Team { get; set; }
+
+	/// <summary>
+	/// Health of the entity
+	/// </summary>
+	[Property]
+	[Range( 0, 100, 10 )]
+	public float MaxHealth { get; set; } = 100f;
+
+
+	private float _health;
+
+	public float Health
+	{
+		get
+		{
+			return _health;
+		}
+
+		set
+		{
+			_health = value;
+		}
+	}
+
+	[Property]
+	public ModelRenderer ModelRenderer { get; set; }
+
+
+	// Methods
 	protected override void OnUpdate()
 	{
 
+	}
+
+	protected override void OnStart()
+	{
+		_health = MaxHealth;
+	}
+
+	protected override void OnFixedUpdate()
+	{
+		
+	}
+
+	protected override void OnDestroy()
+	{
+		
+	}
+
+	private void UpdateHealth(float newHealth)
+	{
+		var difference = newHealth - Health;
+		_health = float.Clamp(newHealth, 0f, MaxHealth );
 	}
 }
